a771c24160d510cad6c4d0d7653178f63e7c0cf7
abelgeorgeantony <abelgeorgeantony@gmail.com>
2024-03-04 19:29:45 +0530

    Now the tokenise() will read words and numbers in the file content and print only them

--- Compiler_cpp/tokeniser.cpp
+++ Compiler_cpp/tokeniser.cpp
@@ -1,8 +1,64 @@
-#include<iostream>
+#include <iostream>
 
 #include "headers/tokeniser.h"
 
+
+bool validNamingChar(char);
+int readWord(char*,std::string*);
+
 void tknsr::tokenise(std::string filecontent)
 {
-    std::cout<<filecontent;
+    std::cout << filecontent<<"\n";
+    int index = 0;
+    std::string word;
+    for(index = 0; index < filecontent.length(); index++)
+    {
+        if(validNamingChar(filecontent.at(index)))
+        {
+            index = index + readWord(&(filecontent.at(index)), &word);
+            std::cout<<word<<"\n";
+        }
+        else
+        {
+            word = "";
+        }
+    }
 }
+
+int readWord(char* word_begin,std::string* word)
+{
+    int i = 0;
+    bool end = false;
+    while (!end)
+    {
+        if (validNamingChar(*(word_begin+i)))
+        {
+            (*word).push_back(*(word_begin+i));
+            i++;
+        }
+        else
+        {
+            end = true;
+            i--; //Sets to the last index of the word
+        }
+    }
+    return i;
+}
+
+bool validNamingChar(char character)
+{
+    if (character >= 'a' && character <= 'z')
+        return true;
+    if (character >= 'A' && character <= 'Z')
+        return true;
+    if (character >= '0' && character <= '9')
+        return true;
+    if (character == '_')
+        return true;
+    
+    return false;
+}
+
+/*bool iskeyword()
+{
+}*/
