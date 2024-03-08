ed5b263be9c8da25f8cad2cf1088bfaa1bad064b
abelgeorgeantony <abelgeorgeantony@gmail.com>
2024-03-08 23:19:48 +0530

    The tokenisation is being implemented little by little

--- Compiler_cpp/headers/tokeniser.h
+++ Compiler_cpp/headers/tokeniser.h
@@ -1,5 +1,23 @@
-#include<iostream>
-namespace tknsr
+#include <iostream>
+#include <vector>
+namespace tknzr
 {
-    void tokenise(std::string filecontent);
+    
+    struct token
+    {
+        std::string data;
+        tokentype type;
+    };
+    struct identifier
+    {
+        std::string name;
+        datatype type;
+    };
+
+    enum tokentype {KEYWORD, IDENTIFIER, OPERATOR, CONSTANT};
+    enum datatype {MOPL_INT, MOPL_FLOAT, MOPL_CHAR};
+    std::vector<std::string> keywordlist = {"if", "else"};
+
+
+    void tokenize(std::string filecontent);
 }
--- Compiler_cpp/tokeniser.cpp
+++ Compiler_cpp/tokeniser.cpp
@@ -1,45 +1,76 @@
 #include <iostream>
+#include <vector>
 
 #include "headers/tokeniser.h"
 
+int readWord(char *, std::string *);
+void assign_wrdtkntype(tknzr::token *);
 
 bool validNamingChar(char);
-int readWord(char*,std::string*);
+bool iskeyword(std::string);
+bool isidentifier(std::string);
 
-void tknsr::tokenise(std::string filecontent)
+std::vector<tknzr::token> tokenlist;
+std::vector<tknzr::identifier> identifierlist;
+
+void tknzr::tokenize(std::string filecontent)
 {
-    std::cout << filecontent<<"\n";
+    std::cout << filecontent << "\n";
     int index = 0;
-    std::string word;
-    for(index = 0; index < filecontent.length(); index++)
+    tknzr::token token;
+    for (index = 0; index < filecontent.length(); index++)
     {
-        if(validNamingChar(filecontent.at(index)))
+        if (validNamingChar(filecontent.at(index)))
         {
-            index = index + readWord(&(filecontent.at(index)), &word);
-            std::cout<<word<<"\n";
+            index = index + readWord(&(filecontent.at(index)), &(token.data));
+            assign_wrdtkntype(&token);
+            tokenlist.push_back(token);
         }
         else
         {
-            word = "";
+            token.data = "";
         }
     }
 }
 
-int readWord(char* word_begin,std::string* word)
+void assign_wrdtkntype(tknzr::token *tkn)
+{
+    if (iskeyword((*tkn).data))
+    {
+        (*tkn).type == tknzr::KEYWORD;
+        return;
+    }
+
+    if (isidentifier((*tkn).data))
+    {
+        (*tkn).type == tknzr::IDENTIFIER;
+        return;
+    }
+    else
+    {
+        tknzr::identifier new_identifier;
+        new_identifier.name = (*tkn).data;
+        identifierlist.push_back(new_identifier);
+        (*tkn).type == tknzr::IDENTIFIER;
+        return;
+    }
+}
+
+int readWord(char *word_begin, std::string *word)
 {
     int i = 0;
     bool end = false;
     while (!end)
     {
-        if (validNamingChar(*(word_begin+i)))
+        if (validNamingChar(*(word_begin + i)))
         {
-            (*word).push_back(*(word_begin+i));
+            (*word).push_back(*(word_begin + i));
             i++;
         }
         else
         {
             end = true;
-            i--; //Sets to the last index of the word
+            i--; // Sets to the last index of the word
         }
     }
     return i;
@@ -55,10 +86,31 @@ bool validNamingChar(char character)
         return true;
     if (character == '_')
         return true;
-    
+
     return false;
 }
 
-/*bool iskeyword()
+bool iskeyword(std::string word)
 {
-}*/
+    int i = 0;
+    for (i = 0; i < tknzr::keywordlist.size(); i++)
+    {
+        if (word == tknzr::keywordlist.at(i))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+bool isidentifier(std::string word)
+{
+    int i = 0;
+    for (i = 0; i < identifierlist.size(); i++)
+    {
+        if (word == identifierlist.at(i).name)
+        {
+            return true;
+        }
+    }
+    return false;
+}
